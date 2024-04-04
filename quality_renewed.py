cdcb7ff7adfefffed8acb72ecd4ab45cf46ef935
janmejoy <janmejoysarker@gmail.com>
2024-04-04 19:17:17 +0530

    Fixed bug. Removed the need for concatenated sun center file.

--- quality_renewed.py
+++ quality_renewed.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Mar 21 15:11:02 2024
-Under Construction
+-Prepared running quality plotter in a more user friendly way.
+-Introduced modules as functions.
+-Quality plot is yet to be introduced.
 @author: janmejoyarch
 """
 import glob
@@ -15,21 +17,16 @@ import os
 from astropy.io import fits
 import matplotlib.pyplot as plt
 
-def concat():
-    sc_files=glob.glob('/home/janmejoyarch/sftp_drive/suitproducts/masterlimbfit/*')
-    sav= '/home/janmejoyarch/temp/'
-    concat_sc= open(sav+'concat_sc.txt', 'w')
-    for file in sc_files:
-        sc= open(file, 'r')
-        lines= sc.read()+'\n'
-        concat_sc.write(lines)
-        sc.close()
-    concat_sc.close()
-
 def suncenter(target):
-    #target= '2024-03-19T04.11.17'
-    sav= '/home/janmejoyarch/temp/'
-    df= pd.read_csv(sav+'concat_sc.txt', sep='\t', header=None, parse_dates=[0])
+    '''
+    Takes time as input.
+    Finds closest sun center value and returns it.
+    '''
+    sc_files=glob.glob('/home/janmejoyarch/sftp_drive/suitproducts/masterlimbfit/*')
+    df= pd.DataFrame()
+    for file in sc_files:        
+        data_df= pd.read_csv(file, sep='\t', header=None, parse_dates=[0])
+        df= pd.concat([df, data_df], ignore_index=True)
     filtered= df[df[5].between(1370, 1430)]
     dt_obj= datetime.strptime(target, '%Y-%m-%dT%H.%M.%S')
     filtered[0]= np.abs(filtered[0]-dt_obj) #filtered based on sun radius
@@ -38,6 +35,9 @@ def suncenter(target):
     return(col, row, rad)
 
 def date_finder():
+    '''
+    Finds common dates between images and sun center files.
+    '''
     masterlimbfit='/home/janmejoyarch/sftp_drive/suitproducts/masterlimbfit/'
     #list all dates for which masterlimbfit exists. Make a set of it.
     masterlimbfit_datelist=set([file.split(".")[0] for file in sorted(os.listdir(masterlimbfit))])
@@ -52,32 +52,43 @@ def date_finder():
     return(datelist)
 
 def image(realpath, col, row, size, plot=None):
+    '''
+    Reads the fits. Plots the image and SC info.
+    Finds mean of a cropped region. Returns mean val.
+    '''
     data= fits.open(realpath)[0].data
     data_crop= data[col-size:col+size, row-size:row+size]
     mean= np.mean(data_crop)
     print(mean)
-    time= datetime.strftime(realpath[-37:-18], '%Y-%m-%dT%H.%M.%S')
+    #time= datetime.strftime(realpath[-37:-18], '%Y-%m-%dT%H.%M.%S')
     if plot==True:
         plt.figure(realpath)
         plt.imshow(data, origin='lower')
         plt.plot(row, col,'o', color='red') #marks center on the sun
+        plt.title(realpath[-64:])
         plt.show()
         plt.close()
-    return(mean, time)
+    return(mean)
 
 
 
-filt='NB07'
+filt='NB03'
 realpath_file_folder= '/home/janmejoyarch/Dropbox/Janmejoy_SUIT_Dropbox/photometry/filelists/'
-realpath_list= [realpath_file_folder+realpath_date+'-realpath.csv' for realpath_date in date_finder()]
+datelist= date_finder()
+realpath_list= [realpath_file_folder+realpath_date+'-realpath.csv' for realpath_date in datelist]
 
 flux_ls, time_ls=[], [] #blank lists
 for realpath_file in realpath_list:
     realpath_file_txt= open(realpath_file, 'r')
     lines= realpath_file_txt.readlines()
-    if (len(lines) != 0):
+    print("Num of lines:", len(lines))
+    if (len(lines) != 0): #check if the file is empty
         realpath_ls=[line[:-1] for line in lines if line.endswith(filt+".fits\n")]
-        realpath= realpath_ls[0]
-        if (int(realpath[-77:-75]) == int(realpath[-29:-27])):
-            row, col, rad= suncenter(realpath[-37:-18])
-            #image(realpath, col, row, 100)
+        if (len(realpath_ls) != 0): #check if list of files is empty
+            print("Num of realpaths:", len(realpath_ls))
+            realpath= realpath_ls[0]
+            if (int(realpath[-77:-75]) == int(realpath[-29:-27])): 
+                #checks if image date and folder date are the same
+                col, row, rad= suncenter(realpath[-37:-18])
+                print(realpath[-64:], row, col, rad)
+                image(realpath, int(col), int(row), 100, plot=True)
