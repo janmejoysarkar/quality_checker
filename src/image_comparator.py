b1ab1061830e21a6b1761b5356c1a6fab7a3aa24
janmejoy <janmejoysarker@gmail.com>
2024-08-25 19:45:06 +0530

    Image comparator- added hist

--- src/image_comparator.py
+++ src/image_comparator.py
@@ -6,11 +6,11 @@ import glob, os
 def fx(hdu, size):
     data= hdu.data
     col, row= int(hdu.header['CRPIX1']), int(hdu.header['CRPIX2']) 
-    data_crop= data[row+2*size:row+4*size,col-size:col+size] #cropped image
+    data_crop= data[row-size:row+size,col-size:col+size] #cropped image
     SUM= np.sum(data_crop)
     return(data_crop, SUM)
     
-s=250
+size=250
 folder1= '/home/janmejoyarch/sftp_drive/suit_data/level1fits/2024/08/10/engg4'
 folder2= '/home/janmejoyarch/sftp_drive/suit_data/level1fits/2024/08/22/engg4'
 
@@ -25,12 +25,19 @@ for filt in filters:
     file1= glob.glob(os.path.join(folder1, f'*{filt}.fits'))[0]
     file2= glob.glob(os.path.join(folder2, f'*{filt}.fits'))[0]
     hdu1= fits.open(file1)[0]
-    crop1= fx(hdu1, 250)
+    crop1= fx(hdu1, size)
     sum_im1_ls.append(crop1[1])
     hdu2= fits.open(file2)[0]
-    crop2= fx(hdu2, 250)
+    crop2= fx(hdu2, size)
     sum_im2_ls.append(crop2[1])
     
+    plt.subplot(3,4,i)
+    plt.hist(hdu1.data.flatten(), bins=100)
+    plt.hist(hdu2.data.flatten(), bins=100, alpha= 0.5)
+    plt.title(filt)
+    i=i+1
+    
+    '''
     plt.subplot(6,4,i)
     plt.imshow(crop1[0], origin='lower')
     plt.colorbar()
@@ -41,8 +48,11 @@ for filt in filters:
     plt.title(f"2024-08-22_{filt}")
     plt.colorbar()
     i=i+1
+    '''
+plt.show()
 
 plt.figure("Sum Plot")
 plt.plot(sum_im1_ls, 'o')
 plt.plot(sum_im2_ls, 'o')
 plt.xticks(ticks=np.arange(11), labels=filters)
+plt.show()
