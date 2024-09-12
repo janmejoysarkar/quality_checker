c170523301bc58da758b870e2921d264265fbebc
janmejoy <janmejoysarker@gmail.com>
2024-09-12 22:11:56 +0530

    image comparater added

--- src/image_comparator.py
+++ src/image_comparator.py
@@ -6,11 +6,11 @@ import glob, os
 def fx(hdu, size):
     data= hdu.data
     col, row= int(hdu.header['CRPIX1']), int(hdu.header['CRPIX2']) 
-    data_crop= data[row-size:row+size,col-size:col+size] #cropped image
+    data_crop= data[row+18*size:row+20*size,col-size:col+size] #cropped image
     SUM= np.sum(data_crop)
     return(data_crop, SUM)
     
-size=250
+size=50
 folder1= '/home/janmejoyarch/sftp_drive/suit_data/level1fits/2024/08/10/engg4'
 folder2= '/home/janmejoyarch/sftp_drive/suit_data/level1fits/2024/08/22/engg4'
 
@@ -22,8 +22,8 @@ sum_im1_ls, sum_im2_ls= [], []
 plt.figure("Crop")
 for filt in filters:
     print(filt)
-    file1= glob.glob(os.path.join(folder1, f'*{filt}.fits'))[0]
-    file2= glob.glob(os.path.join(folder2, f'*{filt}.fits'))[0]
+    file1= sorted(glob.glob(os.path.join(folder1, f'*{filt}.fits')))[5]
+    file2= sorted(glob.glob(os.path.join(folder2, f'*{filt}.fits')))[5]
     hdu1= fits.open(file1)[0]
     crop1= fx(hdu1, size)
     sum_im1_ls.append(crop1[1])
@@ -31,24 +31,23 @@ for filt in filters:
     crop2= fx(hdu2, size)
     sum_im2_ls.append(crop2[1])
     
+    '''
     plt.subplot(3,4,i)
     plt.hist(hdu1.data.flatten(), bins=100)
     plt.hist(hdu2.data.flatten(), bins=100, alpha= 0.5)
     plt.title(filt)
     i=i+1
-    
     '''
-    plt.subplot(6,4,i)
-    plt.imshow(crop1[0], origin='lower')
-    plt.colorbar()
+    plt.subplot(4,6,i)
+    vmn, vmx=0, np.max(crop1[0])-np.max(crop1[0])*0.01
+    plt.imshow(crop1[0], origin='lower', vmin=vmn, vmax=vmx)
     plt.title(f"2024-08-10_{filt}")
     i=i+1
-    plt.subplot(6,4,i)
-    plt.imshow(crop2[0], origin='lower')
+    plt.subplot(4,6,i)
+    plt.imshow(crop2[0], origin='lower', vmin=vmn, vmax=vmx)
     plt.title(f"2024-08-22_{filt}")
-    plt.colorbar()
     i=i+1
-    '''
+    
 plt.show()
 
 plt.figure("Sum Plot")
